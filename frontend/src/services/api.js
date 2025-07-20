cdbdd001efbbde55056fcf096657ebae6af19a94
Aulia Fadjri <17210999@bsi.ac.id>
2025-07-20 17:46:56 +0700

    Add root endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Buat instance axios dengan konfigurasi dasar
 const api = axios.create({
   // URL dasar dari backend API kita
-  baseURL: import.meta.env.VITE_API_BASE_URL ||  'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // Buat "interceptor" untuk permintaan
