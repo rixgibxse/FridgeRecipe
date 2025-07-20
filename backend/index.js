7c713118df31e6d534a55a81d866c0f3fdb8236c
Aulia Fadjri <17210999@bsi.ac.id>
2025-07-20 17:07:36 +0700

    Add root endpoint

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,11 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// Root endpoint
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the FridgeRecipe API!' });
+});
+
 // Routes
 app.use('/api', recipeRoutes);
 app.use('/api/auth', authRoutes);
